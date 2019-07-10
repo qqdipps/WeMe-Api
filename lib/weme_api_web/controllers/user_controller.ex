fe06d57f375cdec799f39f8075b7b9306fb654fb
qqdipps <sdipp007@gmail.com>
2019-07-10 00:20:57 -0700

    removed location URL being sent back with header for create response

--- lib/weme_api_web/controllers/user_controller.ex
+++ lib/weme_api_web/controllers/user_controller.ex
@@ -10,7 +10,6 @@ defmodule WeMeApiWeb.UserController do
     with {:ok, %User{} = user} <- Associates.create_user(user_params) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", Routes.user_path(conn, :show, user))
       |> render("show.json", user: user)
     end
   end
