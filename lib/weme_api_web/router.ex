8f04d97965e679e9673cdd346c9b17b8018067b8
qqdipps <sdipp007@gmail.com>
2019-07-10 00:16:17 -0700

    streamlined CD User routes

--- lib/weme_api_web/router.ex
+++ lib/weme_api_web/router.ex
@@ -7,6 +7,6 @@ defmodule WeMeApiWeb.Router do
 
   scope "/api", WeMeApiWeb do
     pipe_through(:api)
-    resources("/users", UserController, except: [:new, :edit])
+    resources("/users", UserController, only: [:create, :delete])
   end
 end
