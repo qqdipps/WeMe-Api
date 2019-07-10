f9513c67c665025322c937633306f7c4207ff4d1
qqdipps <sdipp007@gmail.com>
2019-07-10 00:28:39 -0700

    culled connection controller to CD operations

--- lib/weme_api_web/controllers/connection_controller.ex
+++ lib/weme_api_web/controllers/connection_controller.ex
@@ -4,32 +4,13 @@ defmodule WeMeApiWeb.ConnectionController do
   alias WeMeApi.Associates
   alias WeMeApi.Associates.Connection
 
-  action_fallback WeMeApiWeb.FallbackController
-
-  def index(conn, _params) do
-    connections = Associates.list_connections()
-    render(conn, "index.json", connections: connections)
-  end
+  action_fallback(WeMeApiWeb.FallbackController)
 
   def create(conn, %{"connection" => connection_params}) do
     with {:ok, %Connection{} = connection} <- Associates.create_connection(connection_params) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", Routes.connection_path(conn, :show, connection))
       |> render("show.json", connection: connection)
-    end
-  end
-
-  def show(conn, %{"id" => id}) do
-    connection = Associates.get_connection!(id)
-    render(conn, "show.json", connection: connection)
-  end
-
-  def update(conn, %{"id" => id, "connection" => connection_params}) do
-    connection = Associates.get_connection!(id)
-
-    with {:ok, %Connection{} = connection} <- Associates.update_connection(connection, connection_params) do
-      render(conn, "show.json", connection: connection)
     end
   end
 
