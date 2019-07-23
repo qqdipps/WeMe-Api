7dd6c2d80a604165186b517f3ac466c27b79bdb3
qqdipps <sdipp007@gmail.com>
2019-07-23 15:46:07 -0700

    removed gettext depend

--- mix.exs
+++ mix.exs
@@ -39,7 +39,6 @@ defmodule WeMeApi.MixProject do
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.1"},
       {:postgrex, ">= 0.0.0"},
-      {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"}
     ]
