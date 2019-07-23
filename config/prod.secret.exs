7871d5a59f9d8a8a37417a01c93d092779f2fd2f
qqdipps <sdipp007@gmail.com>
2019-07-23 15:39:37 -0700

    tbing depoloyment

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -5,7 +5,7 @@
 use Mix.Config
 
 database_url =
-  System.get_env("DATABASE_URL") ||
+  "ecto://postgres:postgres@localhost/weme_api_dev" ||
     raise """
     environment variable DATABASE_URL is missing.
     For example: ecto://USER:PASS@HOST/DATABASE
@@ -17,7 +17,7 @@ config :weme_api, WeMeApi.Repo,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
+  System.get_env(" c") ||
     raise """
     environment variable SECRET_KEY_BASE is missing.
     You can generate one by calling: mix phx.gen.secret
