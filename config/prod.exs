3b13bba80e5757ced01c0bd0a651488eb90667b2
qqdipps <sdipp007@gmail.com>
2019-07-23 12:26:03 -0700

    added build file

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,20 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :weme_api, WeMeApiWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  load_from_system_env: true,
+  http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
+  server: true, # Without this line, your app will not start the web server!
+  secret_key_base: "${SECRET_KEY_BASE}",
+  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
+
+
+  config :weme_api WeMeApi.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: "${DATABASE_URL}",
+  database: "",
+  ssl: true,
+  pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections.
 
 # Do not print debug messages in production
 config :logger, level: :info
